78d915929b3cbb7c3ac43de1d1393fb9125c1fcf
ToCodeABluejay <73219116+ToCodeABluejay@users.noreply.github.com>
2021-11-28 00:19:33 -0500

    Add files via upload

---
+++ rtsrt.py
@@ -0,0 +1,57 @@
+#!/bin/python
+#Released entirely in the Public Domain!! ;)
+srt = input("Path to SRT file: ")
+
+with open(srt, "r") as file:
+	srtl = file.read().split("\n")
+	file.close()
+
+def double_lines(val: int, ll: list) -> bool :
+	print("Found two lines of text in a row. The first one says:")
+	print(ll[val], end="\n\n")
+	print("And the second one says:")
+	print(ll[val+1])
+	x = input("Would you like to delete one? (y/n) ")
+	if x == "y" :
+		return True
+	elif x != "n" :
+		print("Not a listed option.")
+		double_lines(val, ll)
+	else:
+		return False
+		
+
+def del_prompt() -> int :
+	x = input("Would you like to remove the first or the second? (1/2) ")
+	if x == "1" :
+		return 0
+	elif x == "2" :
+		return 1
+	else :
+		print("Not a valid response")
+		del_prompt()
+
+dl = list()
+for i in range(len(srtl)) :
+	if srtl[i] == "\ufeff1" :
+		pass
+	elif srtl[i] != "" and not srtl[i][0].strip("\n/, ->").isnumeric() :
+		if srtl[i+1] != "":
+			k = double_lines(i, srtl)
+			if k :
+				j = del_prompt()
+				dl.append(i+j)
+			else:
+				pass
+for i in dl :
+	del srtl[i]
+
+srtf = open(srt, "w")
+srtf.write(''.join(str(x) for x in srtl))
+
+print("Thank you for using rtsrt! Remember to always double-check your subtitles!! ;)")
+
+
+
+
+
